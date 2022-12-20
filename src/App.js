0d25315dc5ec46b219b0eab2bc3dbfb182af1750
Michael Manley <michaelmanley@Michaels-MacBook-Pro.local>
2022-12-20 14:35:11 -0500

    update to artist and album views

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import AlbumView from './components/AlbumView'
 import ArtistView from './components/ArtistView'
+import { Fragment } from 'react/cjs/react.production.min'
 
 function App() {
 	let [search, setSearch] = useState('')
